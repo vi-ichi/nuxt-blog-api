d032e01724ae85674b1da24c3d104574f8a3ed06
Vi <71643953+vi-ichi@users.noreply.github.com>
2023-08-30 14:58:42 +0000

    data fetching

---
+++ pages/article/[id].vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  async asyncData({ params, $http }) {
+    const article = await $http.$get(
+      `https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/${params.id}`
+    );
+
+    return { article };
+  },
+};
+</script>
+
+<template>
+  <div>Страница статьи</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  async asyncData({ params, $http }) {
+    const articles = await $http.$get(
+      `https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts`
+    );
+
+    return { articles };
+  },
+};
+</script>
+
+<template>
+  <div>Главная страница</div>
+</template>
